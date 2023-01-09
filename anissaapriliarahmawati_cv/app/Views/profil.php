5cbf291d62ecefc22842cdb0e9a05d8932b92170
AnissaApriliaRahmawati <anissaapr12>
2023-01-09 21:40:55 +0700

    modifikasi file cv

---
+++ anissaapriliarahmawati_cv/app/Views/profil.php
@@ -0,0 +1,364 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>iPortfolio Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: iPortfolio - v3.10.0
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/formal.jpeg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Cv Mahasiswa</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+    </div>
+    </div>
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>BIODATA</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/Pramuka.jpeg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Anissa Aprilia Rahmawati</h3>
+            <p class="fst-italic">
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>NIM :</strong> <span>062040832798</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Jurusan:</strong> <span>D4 Manajemen Informatika 2020r</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Instansi:</strong> <span>Politeknik Negeri Sriwijaya</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>No Hp:</strong> <span>082374519482</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>anissaapr12@gmail.com</li>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>TTL:</strong> <span>Sumber Mulia, 12 April 2003</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Umur:</strong> <span>20 Tahun</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Berat Badan:</strong> <span>150 Kg</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Tinggi Badan:</strong> <span>50 CM</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Alamat:</strong> <span>Sumber Asri, Kec.Lubai Ulu, Kab.Muara Enim, Sumsel</span></li>
+                </ul>
+                </ul>
+              </div>
+            </div>
+            <p>
+              Tujuan Hidup : Hidup haruslah punya tujuan ! Prinsip ini harus dimiliki oleh setiap orang, kalau tidak hidupnya akan kehilangan arah. Saya sendiri tentunya harus menetapkan tujuan hidup saya.
+              Walau tujuan hidup saya belum terlau spesifik,tetapi saya telah punya tujuan hidup secara garis besar. Untuk jangka pendek ini, tujuan hidup saya adalah untuk belajar dengan baik, mempelajari
+              banyak hal positif, dan lulus tepat waktu dan di waktu yang tepat dari kampus tercinta ini, Politeknik Negeri Sriwijaya. Tentunya tujuan jangka pendek ini haruslah men-support tujuan jangka
+              menengah hidup saya.
+
+
+              Untuk jangka menengah, saya ingin mendapatkan pekerjaan yang mapan, yang sesuai dengan keahlian dan minat saya. Untuk mendapatkan hal tersebut,oleh karena itu saya harus
+              mensukseskan tujuan jangka pendek hidup saya tadi. Tentunya tidak ada gunanya tujuan hidup tadi jika saya hanya melakukannya untuk diri sendiri. Saya ingin menjadi orang yang bermanfaat bagi
+              orang disekitar saya. Setiap langkah yang saya pilih,saya ingin langkah tersebut memberikan kebahagiaan bagi orang lain.Tentunya setelah mencapai tujuan jangka menengah tadi, saya juga harus
+              berbuat hal yang bisa bermanfaat bagi orang lain. Saya ingin berwirausaha agar dapat memberikan pekerjaan bagi orang lain, karena salah satu masalah terbesar negeri ini adalah pengangguran.
+
+
+              Tujuan jangka panjang hidup saya adalah untuk membahagiakan orang tua saya, dan membuat orang tua saya bangga. Tentunya tujuan tersebut yaitu juga untuk mendapatkan ridho Allah Swt dan masuk
+              surga. Karena kita sebagai manusia nantinya akan kembali kepada-Nya
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+    </div>
+    </div>
+
+    </div>
+
+    </div>
+    </section><!-- End Facts Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+          <p></p>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">Desain <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="80"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Photografer<i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Photoshop <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">Microsoft Word,Excel,Power Poin <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">public speaking<i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p></p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">profil</h3>
+            <div class="resume-item pb-0">
+              <h4>Anissa Aprilia Rahmawati</h4>
+              <p><em></em></p>
+              <ul>
+                <li>Sumber Asri, Kec.Lubai Ulu,Kab.Muara Enim,Sumsel</li>
+                <li>082374519483</li>
+                <li>anissaapr12@gmail.com</li>
+                <li>Instagram : _anissaapr</li>
+              </ul>
+            </div>
+
+            <h3 class="resume-title">Pendidikan</h3>
+            <div class="resume-item">
+              <h4>SDN 12 Lubai</h4>
+              <h4>SMP N 5 Lubai Ulu</h4>
+              <h4>SMA N 1 Lubai Ulu</h4>
+              <h4>Politeknik Negeri Sriwijaya</h4>
+              <p><em></em></p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+          <h3 class="resume-title">Prestasi</h3>
+          <div class="resume-item">
+            <p><em></em></p>
+            <ul>
+              <li>Juara 3 Komputer di Universitas Mahakarya Asia Baturaja (2018)</li>
+              <li>Juara 2 Pembuatan Aplikasi Scout Game Menggunakan Construk 2 Pada LOGIKA Pramuka Polsri Nasional (2021)</li>
+            </ul>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Pengalaman Organisasi</h3>
+            <div class="resume-item">
+              <h4></h4>
+              <p><em></em></p>
+              <ul>
+                <li>OSIS SMA N 1 Lubai Ulu (2018-2020)</li>
+                <li>Pramuka SMA N 1 Lubai Ulu (2018-2020)</li>
+                <li>Kepala Divisi Humas Pramuka Politeknik Negeri Sriwijaya (2021-2022)</li>
+                <li>Pasukan Pengibar Bendera Politeknik Negeri Sriwijaya (2021)</li>
+                <li>Kepala Divisi PSDM Pramuka Politeknik Negeri Sriwijaya (2023-2024)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Lokasi:</h4>
+                <p>Sumber Asri, Kec.Lubai Ulu,Kab.Muara Enim,Sumsel</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>anissaapr12@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>No HP:</h4>
+                <p>082374519482</p>
+              </div>
+
+            </div>
+
+            <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="name">Your Name</label>
+                    <input type="text" name="name" class="form-control" id="name" required>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="name">Your Email</label>
+                    <input type="email" class="form-control" name="email" id="email" required>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="name">Subject</label>
+                  <input type="text" class="form-control" name="subject" id="subject" required>
+                </div>
+                <div class="form-group">
+                  <label for="name">Message</label>
+                  <textarea class="form-control" name="message" rows="10" required></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div>
+
+          </div>
+
+        </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>iPortfolio</span></strong>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
